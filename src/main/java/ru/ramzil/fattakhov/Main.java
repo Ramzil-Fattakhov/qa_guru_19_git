315ea4dc383e6062e20abd8f7a389a53b7b6ddca
Ramzil Fattakhov <135224855+Ramzil-Fattakhov@users.noreply.github.com>
2024-04-14 23:28:49 +0300

    Update Main.java

--- src/main/java/ru/ramzil/fattakhov/Main.java
+++ src/main/java/ru/ramzil/fattakhov/Main.java
@@ -3,7 +3,7 @@ package ru.ramzil.fattakhov;
 
 public class Main {
 
-    privte static String msg = "Hello qa_guru!!!!!!!!!!";
+    private static String msg = "Hello qa_guru!!!!!!!!!";
     
     public static void main(String[] args) {
         System.out.println(msg);
