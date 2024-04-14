794b76e4dc648b6359333eeb98d13e574c7bded5
Ramzil Fattakhov <135224855+Ramzil-Fattakhov@users.noreply.github.com>
2024-04-14 23:32:36 +0300

    Update Main.java

--- src/main/java/ru/ramzil/fattakhov/Main.java
+++ src/main/java/ru/ramzil/fattakhov/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     private static String msg = "Hello qa_guru!!!!!!!!!";
     
-    public static void main(String[] args) {
+    public static void main(String... args) {
         System.out.println(msg);
     }
 }
